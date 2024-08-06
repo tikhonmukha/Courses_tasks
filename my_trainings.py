1a061c13cc07d9aaa869a00e2ab9db53308bb7bb
Tikhon Mukha <tikhonmukha1998@gmail.com>
2024-08-06 22:09:47 +0300

    solved leetcode 1148. Article Views I

--- my_trainings.py
+++ my_trainings.py
@@ -180,3 +180,16 @@ def gameplay_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     return pd.DataFrame({'fraction':[fraction]})
 
 gameplay_analysis(activity)
+
+
+## leetcode 1148. Article Views I
+
+import pandas as pd
+
+data = [[1, 3, 5, '2019-08-01'], [1, 3, 6, '2019-08-02'], [2, 7, 7, '2019-08-01'], [2, 7, 6, '2019-08-02'], [4, 7, 1, '2019-07-22'], [3, 4, 4, '2019-07-21'], [3, 4, 4, '2019-07-21']]
+views = pd.DataFrame(data, columns=['article_id', 'author_id', 'viewer_id', 'view_date']).astype({'article_id':'Int64', 'author_id':'Int64', 'viewer_id':'Int64', 'view_date':'datetime64[ns]'})
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame({'id':views.query('author_id==viewer_id')['author_id'].drop_duplicates().sort_values()})
+
+article_views(views)
